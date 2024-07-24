f764f94d977d3bcbdfa209592c9b005a03821fb7
MojtabaSayadi <141604831+MojtabaSayadi@users.noreply.github.com>
2024-07-24 21:02:43 +0430

    added jwt second

--- src/main/java/security/JwtUtil.java
+++ src/main/java/security/JwtUtil.java
@@ -1,8 +1,20 @@
 package security;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+@Component
 public class JwtUtil {
     @Value("${uni.app.jwtSecret}")
     public String jwtSecret;
@@ -10,15 +22,32 @@ public class JwtUtil {
     public String jwtExpirationMs;
 
     public String generateJwtToken(Authentication authentication){
+        UserDetails userDetails= (User) authentication.getPrincipal(); // override with our userdetail
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date().getTime() )+ jwtExpirationMs))
+                .signWith(key(), SignatureAlgorithm.ES256)
+                .compact();
 
+        ///return "aaaaa";
+    }
+    private Key key(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
 
-        return "aaaaa";
     }
     public String getUserFromJwtToken(String token){
-        return "ali";
+        return  Jwts.parserBuilder().setSigningKey(key())
+                .build().parseClaimsJwt(token).getBody().getSubject();
+
     }
     public boolean validatinJwtToken(String token){
 
-        return true;
+        try {
+            Jwts.parserBuilder().setSigningKey(key()).build().parse(token);
+            return  true;
+        } catch (Exception e){//ExpiredJwtException e)
+            return false;
+        }
     }
 }
